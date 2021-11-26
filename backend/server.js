38223b266bb11c9e112be634c62efcea577e27c0
ShubhamSdubey <shubhamdube090@gmail.com>
2021-11-26 09:42:47 +0530

    node tested

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-
-app.listen(process.env.port, () => 
+app.listen(process.env.port, () =>{
+ 
   console.log(`Server started on port: ${process.env.port} in ${process.env.NODE_ENV} mode.`)
-)
+})
